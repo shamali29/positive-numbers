9470ca06aa9ac931b9e808ba7a1d803c557b662f
shamali29 <92321414+shamali29@users.noreply.github.com>
2021-10-22 21:08:05 +0530

    positive.py
    this file prints positive numbers in the above lists

---
+++ positive.py
@@ -0,0 +1,12 @@
+list1=[12,-7,5,64,-14]
+print("original number in the list: ",list1)
+for num in list1:
+    if num>=0:
+        print(num)
+
+
+list2=[12,14,-95,3]
+print("original number in the list: ",list2)
+for num in list2:
+    if num>=0:
+        print(num)
